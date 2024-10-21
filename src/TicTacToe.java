ab138c637306800506d5b1b17cdefbe305598990
Polina-creator05 <polinager502@gmail.com>
2024-10-21 13:40:49 +0300

    Implement printGameTable function

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -57,5 +57,15 @@ public class TicTacToe {
         printGameTable(mappingTable);
     }
 
+    private static void printGameTable(char[][] gameTable) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("-------------");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + gameTable[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("-------------");
+    }
 
 }
